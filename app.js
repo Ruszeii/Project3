c9c8d0f90675918f2a6900a97b7a07f520a9c361
Ruszeii <123785723+Ruszeii@users.noreply.github.com>
2023-11-09 04:01:32 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const quizData = [
+    {
+        question: "What is 2 + 2?",
+        type: "multiple-choice",
+        options: ["3", "4", "5"],
+        correctAnswer: "4",
+    },
+    {
+        question: "What is the capital of France?",
+        type: "multiple-choice",
+        options: ["London", "Berlin", "Paris"],
+        correctAnswer: "Paris",
+    },
+    {
+        question: "Who wrote 'Romeo and Juliet'?",
+        type: "multiple-choice",
+        options: ["Shakespeare", "Hemingway", "Tolkien"],
+        correctAnswer: "Shakespeare",
+    },
+];
+
